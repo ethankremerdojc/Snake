0eeab692e8a4f6e34d6b08fbb4f7e5c487bb1425
ethankremerdojc <64492390+ethankremerdojc@users.noreply.github.com>
2020-11-09 07:59:36 -0800

    V 1.0 (1 Snake)

---
+++ game.py
@@ -0,0 +1,56 @@
+import pyglet
+from pprint import pprint
+from snake import Snake
+import time
+import random
+
+window_width = 800
+window_height = 800
+window = pyglet.window.Window(width=window_width, height=window_height)
+
+grid = [[[i,j] for j in range(40)] for i in range(40)]
+snake = Snake()
+
+def create_food():
+    global grid, snake
+    on_snake = True
+    print('Attempting to create food')
+    while on_snake == True:
+        random_row = random.choice(grid)
+        random_point = random.choice(random_row)
+        print(f"created point {random_point}")
+        if random_point in snake.position['body'] or random_point == snake.position['head']:
+            print('failed point')
+        else:
+            on_snake = False
+
+    return pyglet.shapes.Rectangle(random_point[0] * 20, random_point[1] * 20, 20, 20, color=(255, 0, 0))
+
+food = create_food()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.RIGHT:
+        snake.direction = 'right'
+    if symbol == pyglet.window.key.LEFT:
+        snake.direction = 'left'
+    if symbol == pyglet.window.key.UP:
+        snake.direction = 'up'
+    if symbol == pyglet.window.key.DOWN:
+        snake.direction = 'down'
+
+def update(dt):
+    global food, window_width, window_height
+    snake.move(snake.direction, window_height, window_width)
+    if snake.position["head"] == [food.x / 20, food.y / 20]:
+        snake.position['body'].append(snake.last_tail_position)
+        food = create_food()
+
+@window.event
+def on_draw():
+    window.clear()
+    snake.draw()
+    food.draw()
+
+pyglet.clock.schedule_interval(update, 0.08)
+pyglet.app.run()
---
+++ snake.py
@@ -0,0 +1,69 @@
+import pprint
+import os
+import sys
+import pickle
+import pyglet
+
+class Snake():
+
+    def __init__(self):
+        self.speed = 2  # blocks per second
+        self.position = {"head": [12, 20], "body": [[11, 20], [10, 20], [9, 20], [8, 20]]}
+        self.direction = "right"
+        self.last_tail_position = [0, 0]
+
+    def update_positions(self, new_position):
+        new_position = {
+            "head": new_position,
+            "body": [self.position['head']] + self.position['body'][:-1]
+        }
+        self.last_tail_position = self.position['body'][-1]
+        self.position = new_position
+
+    def check_for_death(self, position, window_height, window_width):
+        if position in self.position["body"]:
+            self.kill()
+        if position[0] < 0 or position[0] * 20 > window_width or position[1] < 0 or position[1] * 20 > window_height:
+            self.kill()
+
+    def move(self, direction, window_height, window_width):
+
+        new_position = [-1, -1]
+
+        if direction == "left":
+            new_position[0] = self.position["head"][0] - 1
+            new_position[1] = self.position["head"][1]
+
+        if direction == "right":
+            new_position[0] = self.position["head"][0] + 1
+            new_position[1] = self.position["head"][1]
+
+        if direction == "up":
+            new_position[0] = self.position["head"][0]
+            new_position[1] = self.position["head"][1] + 1
+
+        if direction == "down":
+            new_position[0] = self.position["head"][0]
+            new_position[1] = self.position["head"][1] - 1
+
+        self.check_for_death(new_position, window_height, window_width)
+        self.update_positions(new_position)
+
+    def draw(self):
+
+        rectangles = []
+
+        head_position = self.position['head']
+        head_rectangle = pyglet.shapes.Rectangle(head_position[0] * 20, head_position[1] * 20, 20, 20)
+        rectangles.append(head_rectangle)
+
+        for bp in self.position['body']:
+            rect = pyglet.shapes.Rectangle(bp[0] * 20, bp[1] * 20, 20, 20)
+            rectangles.append(rect)
+
+        for rect in rectangles:
+            rect.draw()
+
+    def kill(self):
+        print("Player has died.")
+        exit()
