98dfc67a8222ce3bbeb3e0411ff6a79c45857afe
ethankremerdojc <64492390+ethankremerdojc@users.noreply.github.com>
2020-11-09 08:17:50 -0800

    Completed 2 player version

--- game.py
+++ game.py
@@ -9,48 +9,70 @@ window_height = 800
 window = pyglet.window.Window(width=window_width, height=window_height)
 
 grid = [[[i,j] for j in range(40)] for i in range(40)]
-snake = Snake()
+player1 = Snake({"head": [12, 20], "body": [[11, 20], [10, 20], [9, 20], [8, 20], [7, 20]]})
+player2 = Snake({"head": [12, 10], "body": [[11, 10], [10, 10], [9, 10], [8, 10], [7, 10]]})
 
 def create_food():
-    global grid, snake
+    global grid, player1, player2
     on_snake = True
     print('Attempting to create food')
     while on_snake == True:
         random_row = random.choice(grid)
         random_point = random.choice(random_row)
         print(f"created point {random_point}")
-        if random_point in snake.position['body'] or random_point == snake.position['head']:
+        if random_point in player1.position['body'] or random_point == player1.position['head'] or random_point in player2.position['body'] or random_point == player2.position['head']:
             print('failed point')
         else:
             on_snake = False
 
-    return pyglet.shapes.Rectangle(random_point[0] * 20, random_point[1] * 20, 20, 20, color=(255, 0, 0))
+    return pyglet.shapes.Rectangle(random_point[0] * 20, random_point[1] * 20, 20, 20, color=(255, 100, 100))
 
 food = create_food()
 
 @window.event
 def on_key_press(symbol, modifiers):
+    # Player 1
     if symbol == pyglet.window.key.RIGHT:
-        snake.direction = 'right'
+        player1.direction = 'right'
     if symbol == pyglet.window.key.LEFT:
-        snake.direction = 'left'
+        player1.direction = 'left'
     if symbol == pyglet.window.key.UP:
-        snake.direction = 'up'
+        player1.direction = 'up'
     if symbol == pyglet.window.key.DOWN:
-        snake.direction = 'down'
+        player1.direction = 'down'
+
+    # Player 2
+    if symbol == pyglet.window.key.D:
+        player2.direction = 'right'
+    if symbol == pyglet.window.key.A:
+        player2.direction = 'left'
+    if symbol == pyglet.window.key.W:
+        player2.direction = 'up'
+    if symbol == pyglet.window.key.S:
+        player2.direction = 'down'
 
 def update(dt):
     global food, window_width, window_height
-    snake.move(snake.direction, window_height, window_width)
-    if snake.position["head"] == [food.x / 20, food.y / 20]:
-        snake.position['body'].append(snake.last_tail_position)
+    player1.move(player1.direction, window_height, window_width)
+    player2.move(player2.direction, window_height, window_width)
+
+    player1.check_for_death(window_width, window_height, player2.position)
+    player2.check_for_death(window_width, window_height, player1.position)
+
+    if player1.position["head"] == [food.x / 20, food.y / 20]:
+        player1.position['body'].append(player1.last_tail_position)
+        food = create_food()
+
+    if player2.position["head"] == [food.x / 20, food.y / 20]:
+        player2.position['body'].append(player1.last_tail_position)
         food = create_food()
 
 @window.event
 def on_draw():
     window.clear()
-    snake.draw()
+    player1.draw((100, 100, 255))
+    player2.draw((0, 255, 100))
     food.draw()
 
-pyglet.clock.schedule_interval(update, 0.08)
+pyglet.clock.schedule_interval(update, 0.3)
 pyglet.app.run()
--- snake.py
+++ snake.py
@@ -6,9 +6,9 @@ import pyglet
 
 class Snake():
 
-    def __init__(self):
+    def __init__(self, starting_position):
         self.speed = 2  # blocks per second
-        self.position = {"head": [12, 20], "body": [[11, 20], [10, 20], [9, 20], [8, 20]]}
+        self.position = starting_position
         self.direction = "right"
         self.last_tail_position = [0, 0]
 
@@ -20,10 +20,14 @@ class Snake():
         self.last_tail_position = self.position['body'][-1]
         self.position = new_position
 
-    def check_for_death(self, position, window_height, window_width):
-        if position in self.position["body"]:
+    def check_for_death(self, window_width, window_height, other_player_position):
+        if self.position['head'] in self.position["body"]:
             self.kill()
-        if position[0] < 0 or position[0] * 20 > window_width or position[1] < 0 or position[1] * 20 > window_height:
+
+        if self.position['head'][0] < 0 or self.position['head'][0] * 20 > window_width or self.position['head'][1] < 0 or self.position['head'][1] * 20 > window_height:
+            self.kill()
+
+        if self.position['head'] in other_player_position['body'] or self.position['head'] == other_player_position['head']:
             self.kill()
 
     def move(self, direction, window_height, window_width):
@@ -46,10 +50,9 @@ class Snake():
             new_position[0] = self.position["head"][0]
             new_position[1] = self.position["head"][1] - 1
 
-        self.check_for_death(new_position, window_height, window_width)
         self.update_positions(new_position)
 
-    def draw(self):
+    def draw(self, pcolor):
 
         rectangles = []
 
@@ -58,7 +61,7 @@ class Snake():
         rectangles.append(head_rectangle)
 
         for bp in self.position['body']:
-            rect = pyglet.shapes.Rectangle(bp[0] * 20, bp[1] * 20, 20, 20)
+            rect = pyglet.shapes.Rectangle(bp[0] * 20, bp[1] * 20, 20, 20, color=pcolor)
             rectangles.append(rect)
 
         for rect in rectangles:
